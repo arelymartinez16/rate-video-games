a09f675813496e55a01372fed1256df4895f7119
arelymartinez16 <arelymartinez741@gmail.com>
2024-11-28 00:05:56 -0500

    Worked on landing page

---
+++ app/reviews/layout.jsx
@@ -0,0 +1,12 @@
+export const experimental_ppr = true;
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        {children}
+      </div>
+    </>
+  );
+}
---
+++ app/ui/reviews/Navbar.jsx
@@ -0,0 +1,20 @@
+"use client"
+
+import Link from 'next/link';
+import clsx from 'clsx';
+
+const Navbar = () => {
+    return (
+        <nav className="flex justify-between items-center py-4 bg-gray-800 text-white">
+            <h1 className='text-3xl font-bold'>Game Insider</h1>
+            <div className='flex space-x-4'>
+                <Link href='/' className="flex h-[48px] items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3">Home</Link>
+                <Link href='/about' className='flex h-[48px] items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3'>About</Link>
+                <Link href='/profile' className='flex h-[48px] items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3'>Profile</Link>
+                <Link href='/logout' className='flex h-[48px] items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3'>Logout</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
